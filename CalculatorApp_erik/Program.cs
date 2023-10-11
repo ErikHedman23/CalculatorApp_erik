0cb436bf6f8726f4ddde5a5ca8b6cbcf34430c5b
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-10-11 10:39:59 -0400

    Files added

--- CalculatorApp_erik/Program.cs
+++ CalculatorApp_erik/Program.cs
@@ -3,13 +3,10 @@
 using CalculatorApp_erik;
 //List<ICalculator> methodsStored = new List<ICalculator>();
 
-//bool constance = true;
 
-//do
-//{
     ImplementCalcMethods.UserSelect();
     ImplementCalcMethods.ContinueOrEnd();
 
-//} while (constance);
+
 
 
